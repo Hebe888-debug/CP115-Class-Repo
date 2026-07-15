93881ea80383cf4e8d5593c1b68ecb286d1e158a
Hebe888-debug <hebechong74@gmail.com>
2026-07-15 12:10:08 +0800

    yey

---
+++ labs/lab04/exercise1/lab-4-1.py
@@ -0,0 +1,10 @@
+kwh = int(input())
+if kwh < 100:
+    totalBill = 0.3 * kwh
+else:
+    if kwh > 100 and kwh < 200:
+        totalBill = 0.5 * kwh
+    else:
+        totalBill = 0.75 * kwh
+print(totalBill)
+
