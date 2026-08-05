f302b74493e102a8d6271fbac5f87d9082d6c834
Hebe888-debug <hebechong74@gmail.com>
2026-08-05 12:32:47 +0800

    sfef

--- labs/lab05/exercise5.py
+++ labs/lab05/exercise5.py
@@ -3,9 +3,9 @@ score2 = float(input("Enter the score: "))
 score3 = float(input("Enter the score: "))
 
 totalscore = score1 + score2 + score3
-averagescore = score1 + score2 + score3 /3
+averagescore = (score1 + score2 + score3) / 3
 
-print score1
-print score2
-print score3
-print averagescore
+print(score1)
+print(score2)
+print(score3)
+print(averagescore)1
