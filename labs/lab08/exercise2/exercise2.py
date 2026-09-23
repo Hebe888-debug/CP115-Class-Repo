15f2448076e08eae22ded217f8fc96a40de916c2
Hebe888-debug <hebechong74@gmail.com>
2026-09-23 11:46:27 +0800

    4

--- labs/lab08/exercise2/exercise2.py
+++ labs/lab08/exercise2/exercise2.py
@@ -3,7 +3,31 @@ base_salary = float(input())
 overtime_hours = int(input())
 tax_status = input()
 
+overtime_pay = overtime_hours * 35
+gross_salary = base_salary + overtime_pay
 
+if tax_status == "Single":
+    if gross_salary >= 5000:
+        tax_rate = 0.22
+    else:
+        tax_rate = 0.18
+
+elif tax_status == "Married":
+    if gross_salary >= 6000:
+        tax_rate = 0.20
+    else:
+        tax_rate = 0.15
+
+elif tax_status == "Head":
+    if gross_salary >= 5500:
+        tax_rate = 0.25
+    else:
+        tax_rate = 0.19
+
+income_tax = gross_salary * tax_rate
+epf = gross_salary * 0.11
+socso = gross_salary * 0.005
+net_salary = gross_salary - income_tax - epf - socso
 
 print(employee_name)
 print(tax_rate)
